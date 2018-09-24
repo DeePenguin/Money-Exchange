3b9c74d344858d5aa84f1cab5b47091265550e4b
DeePenguin <_deadpenguin_@mail.ru>
2018-09-24 17:09:47 +0300

    Add a solution of makeExchange function

--- src/index.js
+++ src/index.js
@@ -2,4 +2,30 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    let coinsValue = new Map([['H', 50], ['Q', 25], ['D', 10], ['N', 5], ['P', 1]]);
+    const amountForExchange = {};
+    const notEnoughCoins = {
+        error: "You are rich, my friend! We don't have so much coins for exchange"
+    };
+    //check currency
+    if(currency === 0) {
+        return amountForExchange;
+    } else if(currency > 10000) {
+        return notEnoughCoins;
+    }
+    //calculate 
+    const calculateAmount = (currency, coinsValue) => {
+        for (let [coin, value] of coinsValue){
+            const division = Math.trunc(currency / value);
+            const remainder = currency % value; 
+            if(division > 0) {
+                amountForExchange[coin] = division;
+                currency = remainder;
+            } else if(division > 0 && remainder === 0){
+                break;
+            }
+        }
+        return amountForExchange;
+    };
+    return calculateAmount(currency,coinsValue);
 }
